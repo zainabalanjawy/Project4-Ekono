b792fe38bd515deda1c10186f9161784ddedeb22
zainabalanjawy <109720751+zainabalanjawy@users.noreply.github.com>
2023-07-19 13:56:46 +0300

    Initial setup

---
+++ Project4_Ekono/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Project4_Ekono project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project4_Ekono.settings')
+
+application = get_wsgi_application()
