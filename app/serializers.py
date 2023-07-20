a5443d3d6970def0f15dc2002f6348bc45e7a28d
fatema23 <fmoh20@icloud.com>
2023-07-20 14:26:17 -0700

    category

--- app/serializers.py
+++ app/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 # from .models import Recipet,Category,Expanse
+from .models import Category
 
 # Recipet class serializer 
 # class RecipetSerializer(serializers.ModelSerializer):
 
 
 # Category class serializer 
-# class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 # Expanse class serializer 
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-# from .views import
+from .views import CategoryCreate
+from .views import CategoryDelete
+from .views import CategoryUpdate
 
 urlpatterns=[
+    path('api/category/create/', CategoryCreate.as_view()),
+    path('api/category/<pk>/delete/', CategoryDelete.as_view()),
+    path('api/category/<pk>/update/', CategoryUpdate.as_view()),
     
 ]
