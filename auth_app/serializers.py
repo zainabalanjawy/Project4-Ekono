a837c0652f4462d04d9b82fb189341500ecd932a
Jawaher <jwahr317@gmail.com>
2023-07-22 13:52:07 +0300

    profile added and other backend views

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,17 +1,9 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
-from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(RegisterSerializer, serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", 'email', 'address',
-                  'budget', 'password1', 'password2')
-
-    # def custom_signup(self, request: Request, user: User) -> None:
-    #     for f in self.Meta.fields:
-    #         if hasattr(user, f) and not getattr(user, f):
-    #             setattr(user, f, self.initial_data[f])
-
-    #     user.save()
+        fields = '__all__'
+        # fields = ("id", "username", "first_name", "last_name", "address", "budget", "date_joined", "last_login", "password")
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false) //check if user is logged in or not
-  const [user, setUser] = useState({}) //check if there is such user in the db
+  const [user, setUser] = useState({}) //store user token
   
   useEffect(() => {
     let token = localStorage.getItem('token')
@@ -33,17 +33,15 @@ export default function App() {
   }
 
   const loginHandler = (credintials) => {
-    console.log('login handler entered')
     axios.post('http://127.0.0.1:8000/auth/login/', credintials)
       .then(res => {
-        console.log('xxx')
-        console.log(res.data.key)
+        console.log("user has logged in. Token: ", res.data.key)
         //store the token in our local storage
         let token = res.data.key
         if (token != null) {
           localStorage.setItem("token", token) //create a local storage with the name token
-          // let user = jwt_decode(token) //if it's proper token and not expired then the user is currently logged in
           let user = token
+          // let response = axios.get(`http://127.0.0.1:8000/auth/${credintials['username']}/profile/`)
           setIsAuth(true)
           setUser(user)
           return <Navigate to='/'/>
@@ -53,10 +51,12 @@ export default function App() {
       })
   }
   const logoutHandler = (e) => {
+    console.log('logged in user was: ', user)
     e.preventDefault() //don't execute this code on page refersh
     localStorage.removeItem('token')
     setIsAuth(false)
     setUser(null)
+    console.log('user logged out successfully. Token has been removed')
   }
   return (
     <>
@@ -80,7 +80,7 @@ export default function App() {
           />
           <Route
             path='/profile'
-            element={<Profile/>}
+            element={isAuth ? <Profile user={user} /> : <Signin login={loginHandler} />}
           />
         </Routes>
       </BrowserRouter>
