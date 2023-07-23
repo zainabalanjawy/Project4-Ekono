0c8cd422982a75abd730c588455d82698d7b8c4a
Jawaher <jwahr317@gmail.com>
2023-07-23 09:15:17 +0300

    profile changes

--- front-end/src/components/auth/Profile.js
+++ front-end/src/components/auth/Profile.js
@@ -3,51 +3,46 @@ import axios from 'axios'
 
 export default function Profile(props) {
   //I don't have the id to send the req
-// const response = axios.get(`http://127.0.0.1:8000/auth/${props.user.id}/profile/`)
-// console.log("profile response.data: ", response.data)
-return (
-    <>
-      <div>Profile</div>
+  function handleUser(){
+    const response = axios.get(`http://127.0.0.1:8000/auth/10/profile/`)
+    console.log("profile response.data: ", response)
+    return(
+      <>
       <form>
         <div>
           <label> first Name: </label>
-          <input type='text' name="first_name" placeholder="first name"></input>
+          <input type='text' name="first_name" placeholder="first name" value={response.data.first_name}></input>
         </div>
         <div>
           <label> Last Name: </label>
-          <input type='text' name="last_name" placeholder="last name"></input>
+          <input type='text' name="last_name" placeholder="last name" value={response.data.last_name}></input>
         </div>
         <div>
           <label> Username: </label>
-          <input type='text' name="username" placeholder="username"></input>
+          <input type='text' name="username" placeholder="username" value={response.data.username}></input>
         </div>
         <div>
           <label> Email: </label>
-          <input type="email" name="email" placeholder="example@gmail.com"></input>
+          <input type="email" name="email" placeholder="example@gmail.com" value={response.data.email}></input>
         </div>
         <div>
           <label> Address: </label>
-          <input type='text' name="address" placeholder="first name"></input>
+          <input type='text' name="address" placeholder="first name" value={response.data.address}></input>
         </div>
         <div>
           <label> Budget </label>
-          <input type="number" name="budget" placeholder="your monthly expected income"></input>
+          <input type="number" name="budget" placeholder="your monthly expected income" value={response.data.budget}></input>
         </div>
-        <div>
-          <label>Password: </label>
-          <input type="password" name="password1" placeholder="password"></input>
-        </div>
-        <div>
-          <input type="hidden" name="password"></input>
-        </div>
-        <div>
-          <label>Confirm Password: </label>
-          <input type="password" name="password2" placeholder="password"></input>
-        </div>
-        {/* <button onClick={registerHandler}>Sign Up</button> */}
-        <p>Already have an account? <a href='/signin'>Sign In</a></p>
+        <p><a href='#'>Change Password?</a></p>
+        <button>Edit Profile</button>
       </form>
-
+      </>
+    )
+  }
+return (
+    <>
+      <div>Profile</div>
+      {handleUser}
     </>
   )
 }
