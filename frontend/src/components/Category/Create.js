685f4c0c53e80e8c3675f1b1c2a3961d9b101ed2
zainabalanjawy <109720751+zainabalanjawy@users.noreply.github.com>
2023-07-26 19:37:44 +0300

    Edit categories pages

--- frontend/src/components/Category/Create.js
+++ frontend/src/components/Category/Create.js
@@ -1,6 +1,53 @@
 import React, { useState } from "react";
-import axios from 'axios';
-//import Category from './components/Category/Create'
+import HomeNavbar from "components/Navbars/HomeNavbar.js";
+import FooterHome from "components/Footer/FooterHome.js";
+import classnames from "classnames";
+import axios from 'axios'
+import PerfectScrollbar from "perfect-scrollbar";
+import {BrowserRouter as Router,Navigate, Route , Routes, Link ,useNavigate} from 'react-router-dom'
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  FormText,
+  NavItem,
+  NavLink,
+  Nav,
+  Table,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+  UncontrolledCarousel,
+} from "reactstrap";
+
+const carouselItems = [
+    {
+      src: require("assets/img/denys.jpg"),
+      altText: "Slide 1",
+      caption: "Big City Life, United States",
+    },
+    {
+      src: require("assets/img/fabien-bazanegue.jpg"),
+      altText: "Slide 2",
+      caption: "Somewhere Beyond, United States",
+    },
+    {
+      src: require("assets/img/mark-finn.jpg"),
+      altText: "Slide 3",
+      caption: "Stocks, United States",
+    },
+  ];
+  
+  let ps = null;
+
 export default function Create(props) {
     //Set state for the user into new user
     const [newCatogery, setCatogery] = useState({});
@@ -34,28 +81,91 @@ export default function Create(props) {
 
     return (
         <>
-            <h1> Create Category</h1>
-            <form>
-                <div>
-                    <label>  Category name </label>
-                    <input type='text'name="Category_name" placeholder=" Category_name" onChange={changeHandler}></input>
+     <HomeNavbar />
+     <div className="wrapper">
+        <div className="page-header">
+          <img
+            alt="..."
+            className="dots"
+            src={require("assets/img/dots.png")}
+          />
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path4.png")}
+          />
+          <Container className="align-items-center">
+            <Row>
+              <Col lg="6" md="6">
+                <h1 className="profile-title text-left">Create </h1>
+                <h5 className="text-on-back">Category</h5>
+                <p className="profile-description">
+                Categories in Ekono helps in orginizing expanses and
+                 tracking expanses. Start tracking your expanses by listing 
+                 some categories!
+                </p>
+
+              </Col>
+
+            </Row>
+            <div className="btn-wrapper mb-3">
+                  <p className="category text-success d-inline">
+                    View categories
+                  </p>
+                  <Button tag={Link} to="/Category/List"
+                    className="btn-link"
+                    color="success"
+  
+                    size="sm"
+                  >
+                    <i className="tim-icons icon-minimal-right" />
+                  </Button>
                 </div>
-                <div>
-                    <label> Description </label>
-                    <input type='text' name="Description" placeholder="Description " onChange={changeHandler}></input>
-                </div>
-                <div>
-                    <label> owner: </label>
-                    <input type='text' name="owner" placeholder="owner" onChange={changeHandler}></input>
-                </div>
-                <div>
-                    <label> Emojis </label>
-                    <input type="Emojis" name="Emojis" placeholder="Emojis" onChange={changeHandler}></input>
-                </div>
-              
-                <button onClick={createHandler}> create</button>
-                
-            </form>
-        </>
+          </Container>
+        </div>
+
+
+        <section className="section">
+          <Container>
+          <div class="card">
+          <div class="card-body">
+          <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"> Category name </label>
+      <input name="Category_name" placeholder=" Category_name" onChange={changeHandler} class="form-control" />
+    </div>
+
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Description</label>
+    <input type='text' name="Description" placeholder="Description " onChange={changeHandler} class="form-control" />
+  </div>
+
+  <div class="form-row">
+    {/* <div class="form-group col-md-4">
+      <label for="inputState">Emojies</label>
+      <select type="Emojis" name="Emojis" placeholder="Emojis" onChange={changeHandler} id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div> */}
+    <div class="form-group col-md-2">
+      <label for="inputZip">Emojies</label>
+      <input type="Emojis" name="Emojis" placeholder="Emojis" onChange={changeHandler} class="form-control"/>
+    </div>
+  </div>
+  <Button onClick={createHandler} type="submit" class="btn btn-primary">Create</Button>
+</form>
+</div>
+</div>
+          </Container>
+        </section> 
+
+        <FooterHome />
+      </div>
+
+
+    </>
     )
 }
--- frontend/src/components/Category/List.js
+++ frontend/src/components/Category/List.js
@@ -1,9 +1,68 @@
 import axios from 'axios';
 import React, {useState, useEffect,} from 'react';
-import { Card, Button } from 'react-bootstrap';
+
 import { Link } from 'react-router-dom';
+import HomeNavbar from "components/Navbars/HomeNavbar.js";
+import FooterHome from "components/Footer/FooterHome.js";
 import { useNavigate} from 'react-router-dom'
-const ListCateogry = () => {
+import classnames from "classnames";
+import PerfectScrollbar from "perfect-scrollbar";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  FormText,
+  NavItem,
+  NavLink,
+  Nav,
+  Table,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+  UncontrolledCarousel,
+} from "reactstrap";
+const carouselItems = [
+  {
+    src: require("assets/img/denys.jpg"),
+    altText: "Slide 1",
+    caption: "Big City Life, United States",
+  },
+  {
+    src: require("assets/img/fabien-bazanegue.jpg"),
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States",
+  },
+  {
+    src: require("assets/img/mark-finn.jpg"),
+    altText: "Slide 3",
+    caption: "Stocks, United States",
+  },
+];
+
+let ps = null;
+
+export default function List(props){
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    document.body.classList.toggle("profile-page");
+    // Specify how to clean up after this effect:
+
+  }, []);
     // const list category = () => {
         const [category, setCatogery] = useState([])
         const navigate = useNavigate();
@@ -14,7 +73,7 @@ const ListCateogry = () => {
                 `http://127.0.0.1:8000/api/category/${id}/delete/`
               )
               console.log("deleted successfully!")
-              navigate('/Delete/ViewAll')
+              navigate('/Category/List')
             } catch (error) {
               console.log("Something went wrong", error)
             }
@@ -37,37 +96,235 @@ const ListCateogry = () => {
     // }
     const allCategory = category.map((cat, index) => {
         return (
-          <div class="col-lg-4 col-md-8">
+          <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-              
-              <div class="author">
-                <div class="name">
-                  <span>Cateogry name: {cat.Category_name}</span>
-                  <div class="stats">
-                    <small><i class="far fa-clock"></i>Description: {cat.Description}</small>
-                  </div>
-                </div>
-              </div>
-              <p class="mt-4">Emojes: { cat.Emojis }</p>
-              <p class="mt-4">Owner: { cat.owner }</p>
-              {/* <button type="button" class="btn bg-gradient-primary btn-lg">Details
-              </button> */}
-              <button type="button" class="btn bg-gradient-primary btn-lg" onClick={() => deleteHandler(cat.id)}>Delete</button>
-              {/* <button type="button" class="btn bg-gradient-primary btn-lg" onClick={() => UpdateHandler(cat.id)}>Edit</button> */}
-              
-              
+              <h5 class="card-title">{cat.Category_name}</h5>
+              <p class="card-text">{cat.Description}</p>
+              <p class="card-text">{cat.owner}</p>
+              <Button type="button" color='danger' class="btn bg-gradient-danger btn-lg" onClick={() => deleteHandler(cat.id)}>Delete</Button>
             </div>
           </div>
-          <br></br>
         </div>
+  
+        //   <div class="col-lg-4 col-md-8">
+        //   <div class="card">
+        //     <div class="card-body">
+              
+        //       <div class="author">
+        //         <div class="name">
+        //           <span>Cateogry name: {cat.Category_name}</span>
+        //           <div class="stats">
+        //             <small><i class="far fa-clock"></i>Description: {cat.Description}</small>
+        //           </div>
+        //         </div>
+
+        //         <button type="button" class="btn bg-gradient-primary btn-lg" onClick={() => deleteHandler(cat.id)}>Delete</button>
+        //       </div>
+        //       <p class="mt-4">Emojes: { cat.Emojis }</p>
+        //       <p class="mt-4">Owner: { cat.owner }</p>
+        //       {/* <button type="button" class="btn bg-gradient-primary btn-lg">Details
+        //       </button> */}
+        //       <button type="button" class="btn bg-gradient-primary btn-lg" onClick={() => deleteHandler(cat.id)}>Delete</button>
+        //       {/* <button type="button" class="btn bg-gradient-primary btn-lg" onClick={() => UpdateHandler(cat.id)}>Edit</button> */}
+              
+              
+        //     </div>
+        //   </div>
+        //   <br></br>
+        // </div>
       )
       })
     return (
-    <container>
-        <h1>All Cateogries</h1>
-        {allCategory}
-    </container>
+      <>
+      <HomeNavbar />
+      <div className="wrapper">
+         <div className="page-header">
+           <img
+             alt="..."
+             className="dots"
+             src={require("assets/img/dots.png")}
+           />
+           <img
+             alt="..."
+             className="path"
+             src={require("assets/img/path4.png")}
+           />
+           <Container className="align-items-center">
+             <Row>
+               <Col lg="6" md="6">
+                 <h1 className="profile-title text-left">View All</h1>
+                 <h5 className="text-on-back">Categories</h5>
+                 <p className="profile-description">
+                 Categories in Ekono helps in orginizing expanses and
+                 tracking expanses. Start tracking your expanses by listing 
+                 some categories!
+                 </p>
+ 
+               </Col>
+ 
+             </Row>
+             <div className="btn-wrapper mb-3">
+                   <p className="category text-success d-inline">
+                     Create category
+                   </p>
+                   <Button tag={Link} to="/Category/Create"
+                     className="btn-link"
+                     color="success"
+   
+                     size="sm"
+                   >
+                     <i className="tim-icons icon-minimal-right" />
+                   </Button>
+                 </div>
+           </Container>
+         </div>
+ 
+         {/* <div className="section">
+           <Container>
+             <Row className="justify-content-between">
+               <Col md="6">
+                 <Row className="justify-content-between align-items-center">
+                   <UncontrolledCarousel items={carouselItems} />
+                 </Row>
+               </Col>
+               <Col md="5">
+                 <h1 className="profile-title text-left">Projects</h1>
+                 <h5 className="text-on-back">02</h5>
+                 <p className="profile-description text-left">
+                   An artist of considerable range, Ryan — the name taken by
+                   Melbourne-raised, Brooklyn-based Nick Murphy — writes,
+                   performs and records all of his own music, giving it a warm,
+                   intimate feel with a solid groove structure. An artist of
+                   considerable range.
+                 </p>
+                 <div className="btn-wrapper pt-3">
+                   <Button
+                     className="btn-simple"
+                     color="primary"
+                     href="#pablo"
+                     onClick={(e) => e.preventDefault()}
+                   >
+                     <i className="tim-icons icon-book-bookmark" /> Bookmark
+                   </Button>
+                   <Button
+                     className="btn-simple"
+                     color="info"
+                     href="#pablo"
+                     onClick={(e) => e.preventDefault()}
+                   >
+                     <i className="tim-icons icon-bulb-63" /> Check it!
+                   </Button>
+                 </div>
+               </Col>
+             </Row>
+           </Container>
+         </div>
+         <section className="section">
+           <Container>
+             <Row>
+               <Col md="6">
+                 <Card className="card-plain">
+                   <CardHeader>
+                     <h1 className="profile-title text-left">Contact</h1>
+                     <h5 className="text-on-back">03</h5>
+                   </CardHeader>
+                   <CardBody>
+                     <Form>
+                       <Row>
+                         <Col md="6">
+                           <FormGroup>
+                             <label>Your Name</label>
+                             <Input defaultValue="Mike" type="text" />
+                           </FormGroup>
+                         </Col>
+                         <Col md="6">
+                           <FormGroup>
+                             <label>Email address</label>
+                             <Input placeholder="mike@email.com" type="email" />
+                           </FormGroup>
+                         </Col>
+                       </Row>
+                       <Row>
+                         <Col md="6">
+                           <FormGroup>
+                             <label>Phone</label>
+                             <Input defaultValue="001-12321345" type="text" />
+                           </FormGroup>
+                         </Col>
+                         <Col md="6">
+                           <FormGroup>
+                             <label>Company</label>
+                             <Input defaultValue="CreativeTim" type="text" />
+                           </FormGroup>
+                         </Col>
+                       </Row>
+                       <Row>
+                         <Col md="12">
+                           <FormGroup>
+                             <label>Message</label>
+                             <Input placeholder="Hello there!" type="text" />
+                           </FormGroup>
+                         </Col>
+                       </Row>
+                       <Button
+                         className="btn-round float-right"
+                         color="primary"
+                         data-placement="right"
+                         id="tooltip341148792"
+                         type="button"
+                       >
+                         Send text
+                       </Button>
+                       <UncontrolledTooltip
+                         delay={0}
+                         placement="right"
+                         target="tooltip341148792"
+                       >
+                         Can't wait for your message
+                       </UncontrolledTooltip>
+                     </Form>
+                   </CardBody>
+                 </Card>
+               </Col>
+               <Col className="ml-auto" md="4">
+                 <div className="info info-horizontal">
+                   <div className="icon icon-primary">
+                     <i className="tim-icons icon-square-pin" />
+                   </div>
+                   <div className="description">
+                     <h4 className="info-title">Find us at the office</h4>
+                     <p>
+                       Bld Mihail Kogalniceanu, nr. 8, <br />
+                       7652 Bucharest, <br />
+                       Romania
+                     </p>
+                   </div>
+                 </div>
+                 <div className="info info-horizontal">
+                   <div className="icon icon-primary">
+                     <i className="tim-icons icon-mobile" />
+                   </div>
+                   <div className="description">
+                     <h4 className="info-title">Give us a ring</h4>
+                     <p>
+                       Michael Jordan <br />
+                       +40 762 321 762 <br />
+                       Mon - Fri, 8:00-22:00
+                     </p>
+                   </div>
+                 </div>
+               </Col>
+             </Row>
+           </Container>
+         </section> */}
+         <Row>
+         {allCategory}
+         </Row>
+         <FooterHome />
+       </div>
+ 
+ 
+     </>
     )
 }
-export default ListCateogry  ;
